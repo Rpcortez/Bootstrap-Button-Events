761251a19f3fc2aa49ce627494c0b42d5792a201
Raul <125rperezcortez@frhsd.com>
2024-01-22 14:53:50 -0500

    first commit

---
+++ js/clickEvents.js
@@ -0,0 +1,9 @@
+let btnOne = document.getElementById('click');
+btnOne.addEventListener("click", makeGreen);
+
+function makeGree()
+{
+    console.log("#a0b96b")
+    btnOne.style.background =  "#a0b96b"
+
+}
